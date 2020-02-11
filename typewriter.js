8539b00af1640e44e8ca5521707929456b90c75f
maridoh13 <maridoh@gmail.com>
2020-02-11 20:27:09 +0000

    typewriter animation

---
+++ typewriter.js
@@ -0,0 +1,14 @@
+const sentence = "hello there from lighthouse labs";
+const typewriter = function(sentence) {
+  let timer = 0;
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, timer)
+    timer += 50;
+  }  
+  let linebreak = () => console.log("\n")
+  setTimeout(linebreak, 2000);
+};
+
+typewriter(sentence);
